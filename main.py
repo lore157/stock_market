42f62ad02ba200b828eb61ace8a4414445c4fcf4
Lorenzo Masarin <lorenzo.masarin@student.h-farm.com>
2019-11-27 17:55:21 +0100

    Make a repo a python package

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from stock import get_price
+from mypackage import stock as sck
 
-n,price=get_price("AAPL")
+n,price=sck.get_price("AAPL")
 print("Company {} has a stock value of {}$".format(price, n))
-n,price=get_price("GOOGL")
+n,price=sck.get_price("GOOGL")
 print("Company {} has a stock value of {}$".format(price, n))
 
 
