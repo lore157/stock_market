352135141d2f52f63b6273a6da6571b3c96d9077
M-PETT <matteo.pettena@student.h-farm.com>
2019-11-27 12:23:27 -0500

    document_the_code

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from mypackage import stock as sck
+from mypackage import stock as sck #we import the module stock from mypackage
 
+'''
+main.py works returning us the value of the company's stock price. Now we define the sentences that the program is going to return when called"
+'''
+#we import the function get_price from the module stock
 n,price=sck.get_price("AAPL")
 print("Company {} has a stock value of {}$".format(price, n))
 n,price=sck.get_price("GOOGL")
--- my_package/stock.py
+++ my_package/stock.py
@@ -1,11 +1,11 @@
 import requests
 import json
-
+# we import the libraries that we need to build the function
 
 fmp_URL = 'https://financialmodelingprep.com/api/v3/company/profile/%s'
-
-def get_price(company="AAPL"):
-    r = requests.get(fmp_URL % company)
+#function definition
+def get_price(company="AAPL"):  #we try the function with Apple
+    r = requests.get(fmp_URL % company)#we take the value of teh stock from fmp
     data = json.loads(r.text)
     price = data['profile']['price']
     name = data['profile']['companyName']
