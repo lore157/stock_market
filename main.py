dabb712f56e6cdfb3b76d81301f66635cf1712e3
IvoryTower48 <torresan8@gmail.com>
2019-12-06 04:42:48 -0500

    Implementing argparse function and basic input error messages.

--- main.py
+++ main.py
@@ -11,14 +11,34 @@ main.py works by returning us the value of the company's stock price. Now we def
 #This will check if there are sufficient arguments to make a comparison 
 #between companies
 
-if len(sys.argv) > 2:
-    n, price= sck.get_price(sys.argv[1])
+valid_firms = ["AAPL", "GOOGL"]
+
+def parsing_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("stock_code",
+                        help= "The ticker symbol of the company.",
+                        choices= valid_firms)
+    parser.add_argument("stock_code2",
+                        help= "The ticker symbol of the company.",
+                        choices= valid_firms)
+    parser.add_argument("-v", 
+                        help= "Increases verbosity of the program.",
+                        action= "store_true")
+    args = parser.parse_args()
+    return args
+
+try:
+    args= parsing_input()
+    n, price= stock.get_price(args.stock_code)
     print("Company {} has a stock value of {}$".format(price, n))
-    n, price= sck.get_price(sys.argv[2])
+    n, price= stock.get_price(args.stock_code2)
     print("Company {} has a stock value of {}$".format(price, n))
-    time.sleep(5)
-    exit()
-else:
-    print("Give two arguments in input!")
-    exit()
+
+except:
+    if len(sys.argv)>2:
+        print("Write correctly the arguments!")
+        exit()
+    else:
+        print("Write all the required arguments!")
+        exit()
 
