ed6ad8da4443ee25c872d91e0c113fd25728f189
IvoryTower48 <torresan8@gmail.com>
2019-12-09 08:50:43 -0500

    Conforming program style to PEP-8 standard, for all .py files

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from my_package import stock #Importing module 'stock' from my_package
+from my_package import stock  # Importing module 'stock' from my_package
 import sys
 import time
 import argparse
 import csv
 import json
 
-#main.py works by returning us the value of the company's stock price. Now we define the sentences that the program is going to return when called.
+'''
+"main.py" main objective is to return the value of a company's stock price from
+an online database with real-time data
+'''
 
-#With this function we can read the csv file containing all the allowed valid companies names.
 
+# To read the .csv file containing all the allowed companies names.
 def readCompaniesCsv():
     reader = csv.reader(open('csv_stock.csv', 'r'))
     companies = []
@@ -17,36 +20,39 @@ def readCompaniesCsv():
         companies.append(row[2])
     return companies
 
-valid_firms = readCompaniesCsv()
+'''
+The next function requires two positional arguments to identify the companies
+and an optional argument that allows the user to get more information about the
+status of the program and a more complete output.
+'''
 
-#This function requires two positional arguments to identify the companies and an optional argument that allow the user to get more explicit information about the program"
 
 def parsing_input():
     parser = argparse.ArgumentParser()
     parser.add_argument("stock_code",
-                        help= "The ticker symbol of the company.",
-                        choices= valid_firms)
+                        help "The ticker symbol of the company.",
+                        choices=valid_firms)
     parser.add_argument("stock_code2",
-                        help= "The ticker symbol of the company.",
-                        choices= valid_firms)
-    parser.add_argument("-v", 
-                        help= "Increases verbosity of the program.",
-                        action= "store_true", default= False)
+                        help="The ticker symbol of the company.",
+                        choices=valid_firms)
+    parser.add_argument("-v",
+                        help="Increases verbosity of the program.",
+                        action="store_true", default=False)
     args = parser.parse_args()
     return args
 
-#The following code gives the value of the stock price of the input companies
+# The following code gives the value of the stock price of chosen companies
 try:
-    args= parsing_input()
-    n, price= stock.get_price(args.stock_code)
-#import the function get_price from the module stock
+    valid_firms = readCompaniesCsv()
+    args = parsing_input()
+    n, price = stock.get_price(args.stock_code)  # Calling get_price from stock
     if args.v:
         print("Successfully fetched data")
         print("Company {} has a stock value of {}$".format(price, n))
     else:
         print("{} = {}$".format(price, n))
 
-    n, price= stock.get_price(args.stock_code2)
+    n, price = stock.get_price(args.stock_code2)
 
     if args.v:
         print("Successfully fetched data")
@@ -54,12 +60,13 @@ try:
     else:
         print("{} = {}$".format(price, n))
 
-#This code helps the user to check if he wrote correctly the inputs
+# Troubleshooting for mosto common user errors
 except:
-#This will check if there are sufficient arguments to make a comparison between companies
-    if len(sys.argv)>2:
+    # Check if there are sufficient arguments to allow for a company comparison
+    if len(sys.argv) > 2:
         print("Write correctly the arguments!")
         exit()
     else:
         print("Write all the required arguments!")
         exit()
+
--- my_package/stock.py
+++ my_package/stock.py
@@ -1,14 +1,15 @@
 import requests
 import json
 
-#Importing the libraries needed to build the function
+# Importing the libraries needed to build the function
 fmp_URL = 'https://financialmodelingprep.com/api/v3/company/profile/%s'
 
-#Function definition
+# Function definition
 
-#Will take stock price of a chosen company
-def get_price(company):  
-    r = requests.get(fmp_URL % company)  #Taking value of the stock from fmp
+
+# Retrieving stock price of the chosen company
+def get_price(company):
+    r = requests.get(fmp_URL % company)  # Taking value of the stock from fmp
     data = json.loads(r.text)
     price = data['profile']['price']
     name = data['profile']['companyName']
