5bf1256229f09f02b0c953c30ab5261183b9fd7f
M-PETT <matteo.pettena@student.h-farm.com>
2019-12-14 14:05:10 -0500

    created the test folder and started implenting the tests

---
+++ my_package/test.py
@@ -0,0 +1,22 @@
+import unittest
+from stock_market.main import readCompaniesCsv
+import os
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+    # note: this would be better done with tempfile
+    self.temporary_file = "/tmp/emptyfile"
+    f = open(self.temporary_file, 'w')
+    f.close()
+'''def test_no_datafile(self):
+u, r = parse_allowed_repos(datafile="/tmp/nonexistentfile-wewefwwe")
+self.assertFalse(u)
+self.assertFalse(r)
+def test_empty_datafie(self):
+u, r = parse_allowed_repos(datafile=self.temporary_file)
+self.assertFalse(u)
+self.assertFalse(r)'''
+   def tearDown(self):
+   os.remove(self.temporary_file)
+   setUp()
