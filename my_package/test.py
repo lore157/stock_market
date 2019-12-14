f864652ca68d2053db08dcdc627b5becd2f62348
Lorenzo Masarin <lorenzo.masarin@student.h-farm.com>
2019-12-14 20:28:33 +0100

    implementing tests, but not working

--- my_package/test.py
+++ my_package/test.py
@@ -2,21 +2,62 @@ import unittest
 from stock_market.main import readCompaniesCsv
 import os
 
-class TestMain(unittest.TestCase):
+class TestreadCompaniesCsv(unittest.TestCase):
+    
+    def setUp(self):
+    """creating a temporary file to test our function"""
+        self.temporary_file = "/tmp/emptyfile"
+        f = open(self.temporary_file, 'w')
+        f.close()
+
+    def test_no_datafile(self):
+    """testing with no datafile"""
+        df = readCompaniesCsv(path="/tmp/nonexistentfile-wewefwwe")
+        self.assertFalse(df)        
+
+    def test_empty_datafile(self):
+    """testing with an empty file"""
+        df = readCompaniesCsv(path=self.temporary_file)
+        self.assertFalse(df)
+
+    def test_csv_file(self):
+    """testing a file that is not a csv"""
+        df = readCompaniesCsv(path=self.temporary_file)
+        self.assertFalse(df)
+
+    def tearDown(self):
+    os.remove(self.temporary_file)
+    setUp()
+
+from scripts.dbmanager import check_for_username
+
+class Testdbmanager(unittest.TestCase):
 
     def setUp(self):
-    # note: this would be better done with tempfile
-    self.temporary_file = "/tmp/emptyfile"
-    f = open(self.temporary_file, 'w')
-    f.close()
-'''def test_no_datafile(self):
-u, r = parse_allowed_repos(datafile="/tmp/nonexistentfile-wewefwwe")
-self.assertFalse(u)
-self.assertFalse(r)
-def test_empty_datafie(self):
-u, r = parse_allowed_repos(datafile=self.temporary_file)
-self.assertFalse(u)
-self.assertFalse(r)'''
-   def tearDown(self):
-   os.remove(self.temporary_file)
-   setUp()
+        global conn
+        global cursor
+        conn = sqlite3.connect('credentials.db')
+        cursor = conn.cursor()
+
+    def test_non_existing_user(self):
+    """test with a non existing user"""
+        res = dbmanager.check_for_username("Giulio Cesare", "Ave")
+        self.assertFalse(res)
+
+    def test_empty_user(self):
+    """test with an empty user"""
+        res = dbmanager.check_for_username("", "")
+        self.assertFalse(res)
+
+    def test_mismatched_user(self):
+    """testing with a user with wrong password"""
+        res = dbmanager.check_for_username("Lorenzo", "ciao_sono_lorenzo")
+        self.assertFalse(res)
+
+    def test_correct_user(self):
+    """testing a correct user"""
+        res = github.check_for_username("Lorenzo", "123", 
+                                verbose=True)
+        self.assertTrue(res)
+
+
